8e4a83b83cc8858d065a03baec91c1fe36998b12
Alejo Giraldo <alejito23001@gmail.com>
2026-04-24 14:28:59 -0500

    feat(660): angular introduction

---
+++ 3231660/angular/fundamentos/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+// Función decoradora
+@NgModule({
+  // Declaración de componentes de este módulo
+  declarations: [AppComponent],
+  // Importación de módulos que queremos utilizar
+  imports: [BrowserModule],
+  // Proveedores
+  providers: [],
+  // Es una propiedad que SOLO VA EN EL MÓDULO PRINCIPAL.
+  // Indica cual es el componente incicial.
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
