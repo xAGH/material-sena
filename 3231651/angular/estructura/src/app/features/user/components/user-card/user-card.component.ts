e7af83a473fe26d9b6efc032721891b475d190ba
Alejo Giraldo <alejito23001@gmail.com>
2026-05-22 11:50:35 -0500

    feat: upload angular structure material

---
+++ 3231651/angular/estructura/src/app/features/user/components/user-card/user-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: false,
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.scss'
+})
+export class UserCardComponent {
+
+}
