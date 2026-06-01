1a0a1a5c32568f54d9bc68411a8152ec63550778
Alejo Giraldo <alejito23001@gmail.com>
2026-06-01 15:09:29 -0500

    feat: upload observables example

--- 3231660/angular/observables/src/app/app.module.ts
+++ 3231660/angular/observables/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserModule } from './features/user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UserModule],
+  imports: [BrowserModule, UserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 3231660/angular/observables/src/app/shared/shared.module.ts
+++ 3231660/angular/observables/src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SpinnerComponent],
+  imports: [CommonModule],
+  exports: [SpinnerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
