0a54521fbf8856432ac2eac1ae54f9aade4802b2
Alejo Giraldo <alejito23001@gmail.com>
2026-06-12 12:36:15 -0500

    feat: add angular lifecycle material

--- 3231651/angular/observables/src/app/app.module.ts
+++ 3231651/angular/observables/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostsModule } from './features/posts/posts.module';
+import { EjemploComponent } from './juan/andres/felipe/ejemplo/ejemplo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EjemploComponent],
   imports: [BrowserModule, PostsModule],
   providers: [],
   bootstrap: [AppComponent],
--- 3231660/angular/observables/src/app/app.component.ts
+++ 3231660/angular/observables/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './features/user/models/user.model';
 import { UserService } from './features/user/services/user.service';
 
@@ -7,13 +7,13 @@ import { UserService } from './features/user/services/user.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   users: User[] = [];
   isLoading = false;
 
   constructor(private userService: UserService) {}
 
-  onClick() {
+  ngOnInit(): void {
     this.isLoading = true;
     this.userService.getAllUsers().subscribe({
       next: (data) => {
